3260967b21980ea5372f572ca54c7364b2aafde8
LM-Randomize <lcmurc@gmail.com>
2021-04-25 15:29:42 -0300

    Proyecto solo con el player del dodge the creeps básico, solo para tener el proyecto importado

---
+++ scenes/player/Player.gd
@@ -0,0 +1,35 @@
+extends Area2D
+
+export var speed = 400
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+func _ready():
+	hide()
+
+func _process(delta):
+	var screen_size = get_viewport_rect().size
+	var velocity = Vector2()
+	velocity.x = int(Input.is_action_pressed("derecha")) - int(Input.is_action_pressed("izquierda"))
+	velocity.y = int(Input.is_action_pressed("abajo")) - int(Input.is_action_pressed("arriba"))
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+		
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
