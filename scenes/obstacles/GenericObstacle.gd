b6e30dda8f42562877e256722032efbd40891d97
Tehuel <torresbaldi.tehuel@gmail.com>
2021-06-12 22:09:53 -0300

    adjust randomness

--- scenes/obstacles/GenericObstacle.gd
+++ scenes/obstacles/GenericObstacle.gd
@@ -4,10 +4,10 @@ onready var sprite = $sprite
 onready var collision = $collision
 
 func _ready():
-	self.scale = self.scale * rand_range(0.3, 0.5)
+	self.scale = self.scale * rand_range(0.2, 0.5)
 	
-	var rotation = rand_range(-360, 360)
+	var rotation = rand_range(-30, 30)
 	sprite.rotation_degrees = rotation
 	collision.rotation_degrees = rotation
 	
-	sprite.modulate = Color.from_hsv(rand_range(0.0, 1), 0.15, 1, 1)
+	sprite.modulate = Color.from_hsv(rand_range(0.0, 1), 0.12, 1, 1)
