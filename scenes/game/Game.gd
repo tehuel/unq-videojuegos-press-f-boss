2b49fcc26f90583ab23d189ec490cd0e9e0f2a53
Tehuel <torresbaldi.tehuel@gmail.com>
2021-06-13 13:32:53 -0300

    debug pass level key

--- scenes/game/Game.gd
+++ scenes/game/Game.gd
@@ -9,3 +9,8 @@ func _ready():
 func _process(_delta):
 	if Input.is_action_just_pressed("generate_new_map"):
 		$MapGenerator.generate_random_map()
+
+	if Input.is_action_just_pressed("_debug_pass_map"):
+		Game.set_current_level(Game.level + 1)
+		$MapGenerator.generate_random_map()
+		get_tree().change_scene("res://scenes/game/Game.tscn")
