8c90dbb490512fc1a141b9b5fcdb488ef669de72
ItuFede <federicoituarte1@gmail.com>
2021-06-13 19:07:45 -0300

    add controls to gameplay

--- scenes/mainMenu/CanvasLayer.gd
+++ scenes/mainMenu/CanvasLayer.gd
@@ -1,8 +1,11 @@
 extends CanvasLayer
 
 export var isVisible = true
+export var isInputValid = true
 onready var control = $Control
 
 
 func _ready():
 	control.visible = isVisible
+	control.isInputValid = isInputValid
+	
--- scenes/mainMenu/Control.gd
+++ scenes/mainMenu/Control.gd
@@ -1,8 +1,9 @@
 extends Control
 
+var isInputValid
 
 func _input(event):
-	if event.is_action_pressed("pause"):
+	if event.is_action_pressed("pause") and isInputValid:
 		var new_pause_state = not get_tree().paused
 		get_tree().paused = new_pause_state
 		visible = new_pause_state
