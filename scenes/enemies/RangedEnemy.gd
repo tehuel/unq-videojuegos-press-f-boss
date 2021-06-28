d288dfcb50529f0c86c22060852e981b9bd2a662
LM-Randomize <lcmurc@gmail.com>
2021-06-28 19:02:47 -0300

    Probando hacha en lugar de un "!" (teleg)

--- scenes/enemies/RangedEnemy.gd
+++ scenes/enemies/RangedEnemy.gd
@@ -6,6 +6,8 @@ onready var _telegraph_time = rand_range(0.3, 0.5)
 var _wait_timer = null
 var _pattern_ready = true
 var _telegraph_timer = null
+var telegraph_scene = load("res://scenes/utils/TelegraphAxe.tscn")
+var _telegraph = null
 
 func _ready():
 	_wait_timer = Timer.new()
@@ -18,13 +20,13 @@ func _ready():
 	_telegraph_timer.set_wait_time(_telegraph_time)
 	_telegraph_timer.connect("timeout", self, "_on_telegraph_complete")
 	add_child(_telegraph_timer)
-	$Telegraph.visible = false
 	._ready()
 
 func _attack():
 	if _pattern_ready:
 		_pattern_ready = false
-		$Telegraph.visible = true
+		_telegraph = telegraph_scene.instance()
+		add_child(_telegraph)
 		_telegraph_timer.start()
 
 func _on_timeout_complete():
@@ -32,7 +34,7 @@ func _on_timeout_complete():
 	set_process(true)
 
 func _on_telegraph_complete():
-	$Telegraph.visible = false
+	_telegraph.queue_free()
 	for _i in range(1, _pattern_shoots, 1):
 			weapon.attack()
 			yield(weapon, "attack_ready")
