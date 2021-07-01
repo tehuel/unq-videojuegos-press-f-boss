fa09cdb4e8b56381cd855500566888c1e56fa782
ItuFede <federicoituarte1@gmail.com>
2021-07-01 11:38:39 -0300

    add escape to skip tutorial

--- scenes/environment/Portal.gd
+++ scenes/environment/Portal.gd
@@ -34,6 +34,5 @@ func enable_last():
 
 func _on_Portal_body_entered(body):
 	if body.is_in_group("player"):
-# warning-ignore:return_value_discarded
 		Game.set_current_level(Game.level + 1)
 		get_tree().change_scene("res://scenes/levelSelection/LevelSelection.tscn")
--- scenes/game/Tutorial.gd
+++ scenes/game/Tutorial.gd
@@ -30,6 +30,11 @@ func _ready():
 	portal.enable_fourth()
 	$MeleeEnemy.initialize(self, $AudioDie)
 	$RangedEnemy.initialize(self, $AudioDie)
+	
+func _input(event):
+	if event.is_action_pressed("ui_cancel") && !pause:
+		Game.set_current_level(Game.level + 1)
+		get_tree().change_scene("res://scenes/levelSelection/LevelSelection.tscn")
 
 func _on_enemy_died():
 	enemiesLeft -= 1
@@ -42,31 +47,39 @@ func _on_Player_bloodBackgroundSignal(value):
 
 func _on_Arearclick_body_entered(_body):
 	$Arearclick/Rclick.visible = true
+	$Arearclick/AtaqueRango.visible = true
 
 
 func _on_Arealclick_body_entered(_body):
 	$Arealclick/Lclick.visible = true
+	$Arealclick/AtaqueMele.visible = true
 
 
 func _on_Areaspacebar_body_entered(_body):
 	$Areaspacebar/Spacebar.visible = true
+	$Areaspacebar/Dash.visible = true
 
 
 func _on_Areawasd_body_entered(_body):
 	$Areawasd/Wasd.visible = true
+	$Areawasd/Movimiento.visible = true
 
 
 func _on_Areawasd_body_exited(_body):
 	$Areawasd/Wasd.visible = false
+	$Areawasd/Movimiento.visible = false
 
 
 func _on_Areaspacebar_body_exited(_body):
 	$Areaspacebar/Spacebar.visible = false
+	$Areaspacebar/Dash.visible = false
 
 
 func _on_Arealclick_body_exited(_body):
 	$Arealclick/Lclick.visible = false
+	$Arealclick/AtaqueMele.visible = false
 
 
 func _on_Arearclick_body_exited(_body):
 	$Arearclick/Rclick.visible = false
+	$Arearclick/AtaqueRango.visible = false
