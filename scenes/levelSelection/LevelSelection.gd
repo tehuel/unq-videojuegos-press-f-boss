48e75490bbe2c15b616ed9b03f656cf66dbe1b67
Tehuel <torresbaldi.tehuel@gmail.com>
2021-07-01 19:52:09 -0300

    accelerate level animation

--- scenes/levelSelection/LevelSelection.gd
+++ scenes/levelSelection/LevelSelection.gd
@@ -11,7 +11,7 @@ const animations = {
 	1: "to_level_1",
 	2: "to_level_2",
 	3: "to_level_3",
-	4: "to_valhalla", # not implemented
+	4: "to_valhalla",
 }
 
 func _ready():
@@ -30,21 +30,26 @@ func _ready():
 	
 func _process(_delta):
 	
-	#entro al level cuando termina la animacion
 	var inputEnter = Input.is_action_just_pressed("ui_accept") || Input.is_action_just_pressed("left_click")
-	if (inputEnter && !animation.is_playing()):
-		_start_level(Game.level)
+	
+	if (inputEnter):	
+		# entro al level cuando termina la animacion
+		if (!animation.is_playing()):
+			_start_level(Game.level)
+		# acelero la animacion 
+		else:
+			animation.playback_speed = animation.playback_speed * 1.25
 
 func _start_level(level): 
 	Game.set_current_level(level)
 
 	if(level == end_game):
-		print("end game")
+#		print("end game")
 # warning-ignore:return_value_discarded
 		Game.set_current_level(1)
 		get_tree().change_scene("res://scenes/theEnd/theEnd.tscn")
 	else:
-		print("starting level ", level)
+#		print("starting level ", level)
 # warning-ignore:return_value_discarded
 		get_tree().change_scene("res://scenes/game/Game.tscn")
 		
