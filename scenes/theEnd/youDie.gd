0f3b6898af9bca26705f2e7013e2ec676d8e9eef
Tehuel <torresbaldi.tehuel@gmail.com>
2021-07-02 01:38:17 -0300

    youDie ending

--- scenes/theEnd/youDie.gd
+++ scenes/theEnd/youDie.gd
@@ -9,10 +9,23 @@ func _ready():
 	audio_player_die.set_stream(audio_player_die.player_die[numberSound])
 	audio_player_die.set_volume_db(-10.0)
 	audio_player_die.play()
-	yield(audio_player_die, "finished")
+#	yield(audio_player_die, "finished")
 # warning-ignore:return_value_discarded
-	Game.set_current_level(1)
-	get_tree().change_scene("res://scenes/main/Main.tscn")
+
+func _process(_delta):
+	var inputEnter = Input.is_action_just_pressed("ui_accept") || Input.is_action_just_pressed("left_click") || Input.is_action_just_pressed("ui_cancel")
+
+
+	if (inputEnter):	
+		# entro al level cuando termina la animacion
+		if (!$AnimationPlayer.is_playing()):
+			Game.set_current_level(1)
+			get_tree().change_scene("res://scenes/mainMenu/MainMenu.tscn")
+
+		# acelero la animacion 
+		else:
+			$AnimationPlayer.playback_speed = $AnimationPlayer.playback_speed * 1.5
+
 
 func randomNumberBetween(numberOne, numberTwo):
 	rng.randomize()
